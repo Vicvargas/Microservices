1e57b0a2491a340734f41c2cfccc7d9652a6ef3c
josecespedesant <josecespedesant@gmail.com>
2019-11-19 04:04:37 -0600

    Primer commit

---
+++ complete/src/main/java/CompressionMicroservice/Application.java
@@ -0,0 +1,19 @@
+package CompressionMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+//@ComponentScan(basePackages = {"hello.controller.request"})
+//@ComponentScan(basePackages = {"hello.controller.response"})
+
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
