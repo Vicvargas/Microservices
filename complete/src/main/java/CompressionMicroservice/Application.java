b9b636b612e5025d31d424fc1ee91e6c5fa09cd7
josecespedesant <josecespedesant@gmail.com>
2019-11-21 06:03:39 -0600

    Comunicación con React JS

--- complete/src/main/java/CompressionMicroservice/Application.java
+++ complete/src/main/java/CompressionMicroservice/Application.java
@@ -4,13 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 
-//@ComponentScan(basePackages = {"hello.controller.request"})
-//@ComponentScan(basePackages = {"hello.controller.response"})
+@ComponentScan(basePackages = {"CompressionMicroservice.controller.request"})
+@ComponentScan(basePackages = {"CompressionMicroservice.controller.response"})
 
 public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- complete/src/main/java/CompressionMicroservice/controller/EncoderController.java
+++ complete/src/main/java/CompressionMicroservice/controller/EncoderController.java
@@ -5,21 +5,69 @@ import CompressionMicroservice.controller.response.EncodeResponse;
 import CompressionMicroservice.service.EncodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST})
+
 public class EncoderController {
 
     @Autowired
     private EncodeService encodeService;
 
-    @RequestMapping(value = "/encode", method = RequestMethod.GET)
-    public ResponseEntity<EncodeResponse> encode(@RequestParam(value = "text") String text) {
-        EncodeResponse response = encodeService.encode(text);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    @RequestMapping(value = "/encode", method = {RequestMethod.POST})
+    public ResponseEntity<EncodeResponse> encode(@RequestParam("file") MultipartFile file) throws IOException {
+
+        File convFile = new File(file.getOriginalFilename());
+        convFile.createNewFile();
+
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+
+        System.out.printf("File name = %s, size = %s\n", file.getOriginalFilename(),file.getSize());
+
+        return ResponseEntity.ok().build();
     }
+
+    @RequestMapping(value = "/encode", method = {RequestMethod.GET})
+    public ResponseEntity<EncodeResponse> encodeGet(@RequestParam(value = "text") String text){
+        List<String> records = new ArrayList<String>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("/home/josecespedesant/Microservices/CompressionMicroservice/Microservices/gs-spring-boot-docker/complete/"+text));
+            String line;
+            while((line = reader.readLine()) != null){
+                records.add(line);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String finalstring = "";
+
+        for(int i=0; i<records.size(); i++){
+            finalstring += records.get(i);
+            finalstring += "æ";
+        }
+
+        EncodeResponse response = encodeService.encode(finalstring);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+
+
 }
